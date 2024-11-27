4cf703d0a1cd8dad8a36f6bc4aec223b7df7318d
Jonathan Becerro Martin <jonathan.becerrom@viewnext.com>
2024-11-27 10:32:11 +0100

    Fin de seccion 4

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  standalone: false,
+
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  public heroeNames: string[] = ["Spiderman", "Ironman", "Hulk", "Thor"];
+  public deletedHero?: string;
+
+  clearLastHero ():void {
+
+    this.deletedHero = this.heroeNames.pop();
+  }
+
+}
